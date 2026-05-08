8d07e07f7ccfc1b8a0fa3677dccd121b3c512d3a
gabijerecelroa <gabijerecel@gmail.com>
2026-05-07 23:52:09 -0300

    Bump release version to 1.0.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.storetd.play"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         buildConfigField(
             "String",
