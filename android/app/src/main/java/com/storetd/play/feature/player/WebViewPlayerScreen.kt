8fe5c6099b4ccd7e198045e89719742f480b114a
StoreTD Admin <admin@storetdplay.com>
2026-06-01 09:13:27 +0100

    Fix WebView player AndroidView import

--- android/app/src/main/java/com/storetd/play/feature/player/WebViewPlayerScreen.kt
+++ android/app/src/main/java/com/storetd/play/feature/player/WebViewPlayerScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import kotlinx.coroutines.delay
 
 private fun hostOf(url: String): String {
