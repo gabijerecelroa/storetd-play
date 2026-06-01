0bb861628f687e9d268245e0e4cb62daf8bc6e72
StoreTD Admin <admin@storetdplay.com>
2026-06-01 09:06:39 +0100

    Release v1.6.17 improve movie web player

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.storetd.play"
         minSdk = 23
         targetSdk = 36
-        versionCode = 66
-        versionName = "1.6.16"
+        versionCode = 67
+        versionName = "1.6.17"
 
         buildConfigField(
             "String",
