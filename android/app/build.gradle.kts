425927fd8b8294ae14072c136fd88415333cacb1
StoreTD Admin <admin@storetdplay.com>
2026-06-01 08:33:06 +0100

    Release v1.6.16 external movie player

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.storetd.play"
         minSdk = 23
         targetSdk = 36
-        versionCode = 65
-        versionName = "1.6.15"
+        versionCode = 66
+        versionName = "1.6.16"
 
         buildConfigField(
             "String",
