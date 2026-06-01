900a16f9fec676952518993f102f9a51f0c4bf3e
StoreTD Admin <admin@storetdplay.com>
2026-06-01 06:42:34 +0100

    Release v1.6.13 fix manual Magma refresh build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.storetd.play"
         minSdk = 23
         targetSdk = 36
-        versionCode = 62
-        versionName = "1.6.12"
+        versionCode = 63
+        versionName = "1.6.13"
 
         buildConfigField(
             "String",
