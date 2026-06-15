fcf493130b9a369551c4955572ff696f430fc36d
StoreTD Admin <admin@storetdplay.com>
2026-06-15 20:40:27 +0100

    🚀 FINAL PRO V6: Menú Lateral Premium Recuperado (v1.6.55)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.storetd.play"
         minSdk = 23
         targetSdk = 36
-        versionCode = 90
-        versionName = "1.6.54"
+        versionCode = 91
+        versionName = "1.6.55"
 
         buildConfigField(
             "String",
